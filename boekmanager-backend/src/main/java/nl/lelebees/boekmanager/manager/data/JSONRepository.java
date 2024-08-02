048be91a184bcb3b0c787964301d0da86146b0c5
Lelebees <Lelebees@protonmail.com>
2024-08-02 11:11:38 +0200

    Dried up code

--- boekmanager-backend/src/main/java/nl/lelebees/boekmanager/manager/data/JSONRepository.java
+++ boekmanager-backend/src/main/java/nl/lelebees/boekmanager/manager/data/JSONRepository.java
@@ -1,21 +1,41 @@
 package nl.lelebees.boekmanager.manager.data;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.lelebees.boekmanager.manager.domain.Entity;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public abstract class JSONRepository<Type extends Entity<ID>, ID> implements Repository<Type, ID> {
-
     protected final List<Type> allTypes;
+    protected final Class<Type> clazz;
 
-    public JSONRepository(List<Type> allTypes) {
+    protected final ObjectMapper mapper;
+    protected final Path path;
+
+    public JSONRepository(List<Type> allTypes, Class<Type> clazz) {
         this.allTypes = allTypes;
+        this.clazz = clazz;
+        this.path = Paths.get(clazz.getSimpleName() + ".json");
+        this.mapper = new ObjectMapper();
+        if (!Files.exists(path)) {
+            try {
+                Files.createFile(path);
+                Files.writeString(path, "[]");
+            } catch (IOException e) {
+                throw new RuntimeException("Could not create File for saving " + clazz.getSimpleName() + "s: " + e.getMessage(), e);
+            }
+        }
     }
 
-    public JSONRepository() {
-        this(new ArrayList<>());
+    public JSONRepository(Class<Type> clazz) {
+        this(new ArrayList<>(), clazz);
     }
 
     @Override
@@ -31,6 +51,14 @@ public abstract class JSONRepository<Type extends Entity<ID>, ID> implements Rep
     @Override
     public Type save(Type entity) {
         allTypes.add(entity);
+        try {
+            String text = mapper.writeValueAsString(allTypes);
+            Files.writeString(path, text);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Could not save " + clazz.getSimpleName() + ". Something went wrong parsing to JSON.", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not save " + clazz.getSimpleName() + ". Could not write to file.", e);
+        }
         return entity;
     }
 
--- boekmanager-backend/src/main/java/nl/lelebees/boekmanager/manager/data/book/JSONBookRepository.java
+++ boekmanager-backend/src/main/java/nl/lelebees/boekmanager/manager/data/book/JSONBookRepository.java
@@ -2,66 +2,36 @@ package nl.lelebees.boekmanager.manager.data.book;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.lelebees.boekmanager.manager.data.JSONRepository;
 import nl.lelebees.boekmanager.manager.domain.book.Book;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class JSONBookRepository extends JSONRepository<Book, UUID> implements BookRepository {
 
-    private final ObjectMapper mapper;
-    private final String url = "Book.json";
-    private final Path path = Paths.get(url);
-
     public JSONBookRepository() {
-        super();
-        this.mapper = new ObjectMapper();
-        if (!Files.exists(path)) {
-            try {
-                Files.createFile(path);
-                Files.writeString(path, "[]");
-            } catch (IOException e) {
-                throw new RuntimeException("Could not create File for saving Books: " + e.getMessage(), e);
-            }
-        }
-        List<Book> books;
+        super(Book.class);
+        List<Book> types;
         try {
             String content = Files.readString(path);
-            books = mapper.readValue(content, new TypeReference<>() {
+            types = mapper.readValue(content, new TypeReference<>() {
             });
         } catch (JsonProcessingException e) {
-            System.out.println("Something went wrong parsing JSON!");
+            System.out.println("Something went wrong parsing JSON for " + clazz.getSimpleName() + "!");
             System.out.println(e);
-            books = new ArrayList<>();
+            types = new ArrayList<>();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Something went wrong writing to file.", e);
         }
-        super.allTypes.addAll(books);
-    }
-
-    @Override
-    public Book save(Book entity) {
-        super.save(entity);
-        try {
-            String text = mapper.writeValueAsString(super.allTypes);
-            Files.writeString(path, text);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Could not save book. Something went wrong parsing to JSON.", e);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not save book. Could not write to file.", e);
-        }
-        return entity;
+        allTypes.addAll(types);
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return new ArrayList<>(super.allTypes);
+        return new ArrayList<>(allTypes);
     }
 }
